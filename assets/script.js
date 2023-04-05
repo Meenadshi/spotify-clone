1cc149599ff85c4568e23f69fcfd48f4036b3ad8
Meenadshi <meenadshi20@gmail.com>
2023-04-05 22:20:29 +0530

    -Add.Functionality to play the first song was created

---
+++ assets/script.js
@@ -0,0 +1,30 @@
+console.log("Welcome to Spotify-Clone");
+let songIndex = 0;
+let audioElement = new Audio('/assets/audio/audio1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let progressBar = document.getElementById('progressBar');
+let gif = document.getElementById('gif');
+let songs = [
+    {songName: "It's always blue", filePath: "/assets/audio/audio1.mp3", coverPath: "/assets/image/cover1.jpg"},
+    {songName: "Trap", filePath:     "/assets/audio/audio2.mp3", coverPath: "/assets/image/cover2.jpg"},
+    {songName: "They mad", filePath: "/assets/audio/audio3.mp3", coverPath: "/assets/image/cover3.jpg"},
+    {songName: "Rich the kid", filePath: "/assets/audio/audio4.mp3", coverPath: "/assets/image/cover4.jpg"}
+]
+
+masterPlay.addEventListener('click', () =>{
+    if(audioElement.paused || audioElement.currentTime <= 0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+})
+
+progressBar.addEventListener('timeupdate', () =>{
+    console.log('timeupdate')
+})
